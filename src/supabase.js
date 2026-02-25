8594681d594b120a87b9871218c2853c9434992f
wizardleeen <wizardleen@gmail.com>
2026-02-25 16:46:51 +0800

    更新 Supabase 配置

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://voyxrzvueadloyzcpebk.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZveXhyenZ1ZWFkbG95emNwZWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NDc0MzIsImV4cCI6MjA4NzUyMzQzMn0.i85WF9oeT2hkncXjELuFfNonYxN7Gz73UzseopZkP3M'
+const supabaseUrl = 'https://febpplzuipsowruqhkgg.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlYnBwbHp1aXBzb3dydXFoa2dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMDAzODAsImV4cCI6MjA4NzU3NjM4MH0.hBvTSGKrv6moniKWeK_5kGbRkHnyB6JMv1yJ_uPGlp0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
